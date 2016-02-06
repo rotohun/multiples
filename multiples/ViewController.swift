746b99d6f679872b3b5bd20ea731c2461ae3a03b
Ronald Hunter <ronald@ronaldhunter.co>
2016-02-05 19:22:25 -0500

    Made the functions and linked avriables for the app to work and now can be extended if need be.

--- multiples/ViewController.swift
+++ multiples/ViewController.swift
@@ -9,17 +9,86 @@
 import UIKit
 
 class ViewController: UIViewController {
+    //Global variables
+    
+    var multiple = 0
+    var counter = -1
+    var sum = 0
+    
+    //Home Screen
+        //Variables
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        @IBOutlet weak var appTitle: UIImageView!
+    
+        @IBOutlet weak var multipleInsert: UITextField!
+    
+        @IBOutlet weak var playButton: UIButton!
+    
+    
+    
+    
+    
+    //Add Screen
+        //Variables
+    
+        @IBOutlet weak var mathLnl: UILabel!
+        @IBOutlet weak var addButton: UIButton!
+    
+    
+    
+    
+    
+    
+        @IBAction func playButton(sender: AnyObject) {
+            if multipleInsert.text != nil && multipleInsert.text != ""{
+             
+                shouldBehidden(true)
+            }
+        }
+    
+    
+    @IBAction func addButton(sender: AnyObject) {
+        
+        multiple = Int(multipleInsert.text!)!
+        
+        
+        
+        counter++
+        
+        var n1 = counter * multiple
+        
+        sum = multiple + n1
+        
+        mathLnl.text = "\(n1) + \(multiple) = \(sum)"
+        
+    }
+    
+    
+    // Functions 
+    
+    
+    func shouldBehidden(check:Bool){
+        if check == true{
+            
+            playButton.hidden = true
+            multipleInsert.hidden = true
+            appTitle.hidden = true
+            
+            mathLnl.hidden = false
+            addButton.hidden = false
+            
+        }else{
+            
+            playButton.hidden = false
+            multipleInsert.hidden = false
+            appTitle.hidden = false
+            
+            mathLnl.hidden = true
+            addButton.hidden = true
+            
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
 
